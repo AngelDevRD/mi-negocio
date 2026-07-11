ff0b86a31bf0e77b6bc4500f23261fd38728347f
Angel <angeldnielgs@gmail.com>
2026-07-11 15:45:53 -0400

    feat: agrega actualizador in-app (consulta version, muestra dialogo, abre el APK para instalar)

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'core/app_updater.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
+
+bool _updateCheckStarted = false;
 
 class AppGestion extends ConsumerWidget {
   const AppGestion({super.key});
@@ -15,6 +18,17 @@ class AppGestion extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
       routerConfig: router,
+      builder: (context, child) {
+        if (!_updateCheckStarted) {
+          _updateCheckStarted = true;
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (context.mounted) {
+              AppUpdater.checkForUpdate(context, slug: 'mi-negocio');
+            }
+          });
+        }
+        return child!;
+      },
     );
   }
 }
